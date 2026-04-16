10d8336dfd746daa714d8d75d13e75475259131f
Daniel cato <danielcato038@gmail.com>
2026-04-16 04:30:16 +0000

    fix: use simplest WhiteNoise storage to bypass build failures on missing admin files

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -152,7 +152,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.StaticFilesStorage",
     },
 }
 
